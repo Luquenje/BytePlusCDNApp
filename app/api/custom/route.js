3abe663dcb386ed24eca8971fb84373f0450e79f
NickAdveronTech <nicholaslau123@gmail.com>
2025-06-19 05:41:51 +0800

    custom route

--- app/api/custom/route.js
+++ app/api/custom/route.js
@@ -1,12 +1,23 @@
-export async function GET(req: Request) {
-  const headersObj: any = {};
-  // Convert Headers object to plain object
-  req.headers.forEach((value, key) => {
-    headersObj[key] = value;
-  });
+import { NextResponse } from 'next/server';
+import path from 'path';
+import { promises as fs } from 'fs';
 
-  return new Response(JSON.stringify(headersObj), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+export async function GET() {
+  // Absolute path to the file
+  const filePath = path.join(process.cwd(), 'public', 'file.svg');
+  console.log(filePath)
+  try {
+    // Read file content
+    const fileContent = await fs.readFile(filePath);
+
+    // Create response with appropriate headers
+    return new NextResponse(fileContent, {
+      headers: {
+        'Content-Type': 'text/plain',
+        'Content-Disposition': 'attachment; filename="file.svg"',
+      },
+    });
+  } catch (error) {
+    return NextResponse.json({ error: 'File not found' }, { status: 404 });
+  }
 }
